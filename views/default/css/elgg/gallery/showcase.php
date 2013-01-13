2a547175906d558df728aa9a731d118488a6be9c
Evan Winslow <evan@elgg.org>
2013-01-13 15:43:12 -0800

    More robust 1px separator

--- views/default/css/elgg/gallery/showcase.php
+++ views/default/css/elgg/gallery/showcase.php
@@ -1,5 +1,7 @@
 .elgg-gallery-showcase {
-    margin: 0 1em;
+    margin: 0;
+    border: 1px solid #EEE;
+    border-width: 1px 0 0 1px;
 }
 
 .elgg-gallery-showcase > li {
@@ -7,6 +9,6 @@
     display: inline-block;
     box-sizing: border-box;
     border: 1px solid #EEE;
-    margin: -1px 0 0 -1px;
+    border-width: 0 1px 1px 0;
     padding: .5em;
 }
